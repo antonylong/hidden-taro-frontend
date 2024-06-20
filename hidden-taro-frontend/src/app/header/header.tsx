398a1cabc0bc97c4b9dedb8fa5770b7000b5346e
Antony Long <antonylong@Antonys-MacBook-Pro.local>
2024-06-20 15:26:54 +0100

    updated some todos in header file

--- hidden-taro-frontend/src/app/header/header.tsx
+++ hidden-taro-frontend/src/app/header/header.tsx
@@ -4,9 +4,12 @@ const Header = () => {
   // TODO: update the company title to be Kevin's title
   return (
     <header className="flex justify-between pb-90 px-16">
+      {/* TODO: Company title should bring back to homepage */}
       <h1 className="text-3xl">Company Title</h1>
       <div className="flex w-800 justify-between">
+        {/* TODO: navigate to pages */}
         <p className="pr-10">About</p>
+        {/* TODO: contact brings to bottom */}
         <p>Contact</p>
       </div>
     </header>
