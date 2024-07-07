859139a69249daa52f27bd1306a343e28d7c2018
antony <countalong@gmail.com>
2024-07-07 17:43:08 +0100

    update: remove mvp page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
 import Header from "../views/header/header";
 import LandingPage from "../views/landingPage/landingPage";
-import MvpPage from "../views/mvpPage/mvpPage";
 import SubPage from "../views/subPage/subPage";
 
 export default function Page() {
   return (
-    <main className="flex min-h-screen flex-col pt-10 w-full bg-black">
+    <main className="flex min-h-screen flex-col pt-10 w-full">
       <Header />
-      <MvpPage />
+      <LandingPage />
+      <SubPage />
     </main>
   );
 }
